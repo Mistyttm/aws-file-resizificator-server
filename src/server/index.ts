5e0e699c2ab00b2cd5ff40c8a94eaabe4e017350
Mistyttm <emmey.leo@gmail.com>
2023-10-12 13:03:53 +1000

    commenting

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,14 +1,17 @@
+// Imports
 import express from 'express';
 const app = express();
 import path from 'path';
 import cors from 'cors';
 import morgan from 'morgan';
 
+// CommonJS imports
 //@ts-ignore
 const testRouter = require("./routes/test");
 
 const PORT = process.env.PORT ?? 8080;
 
+// Middleware
 app.use(express.static(path.join(__dirname, "..", "..", "build", "app")));
 app.use(express.static("public"));
 app.use(cors());
@@ -16,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("combined"));
 
+// Routing
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "..", "..", "build", "app", "index.html"));
 });
