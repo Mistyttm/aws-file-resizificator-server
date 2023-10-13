ed3a1c753db01729aa1fdf96388a38ac5bd03bbb
Mistyttm <emmey.leo@gmail.com>
2023-10-13 20:39:40 +1000

    FFMPEG middleware beginning

---
+++ src/server/middleware/ffmpeg.ts
@@ -0,0 +1,8 @@
+import { UploadedFile } from "express-fileupload";
+// import Ffmpeg from "fluent-ffmpeg";
+
+// this will do the resolution conversions
+// TODO: Update the resolution type
+async function convertVideoRes(videoFile: UploadedFile, resolution: string) {
+    console.log("Hello World");
+}
