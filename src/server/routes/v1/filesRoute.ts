dbc764956695c06a32ca8d525d0e1b9c9e0f53d7
Mistyttm <emmey.leo@gmail.com>
2023-10-13 23:05:37 +1000

    Functioning file uploader

--- src/server/routes/v1/filesRoute.ts
+++ src/server/routes/v1/filesRoute.ts
@@ -1,23 +1,13 @@
 import { Router } from "express";
-import path from "path";
-import { UploadedFile } from 'express-fileupload'
-
-import getVideo from "../../middleware/video";
+import upload from "../../middleware/multer"
 
 export const filesRouter = Router();
 
 /* Upload video file from client */
 // These req and res types are what vs code added in for now to make typescript stop complaining about implicit types
-filesRouter.post("/uploadFile", async (req, res) => {
-    const videoFile = req.files?.video as UploadedFile;
-
-    try {
-        // Get the path of the imported file
-        const filePath = path.join(__dirname, '..', '..', 'videos', videoFile.name);
-
-        // Call the function to store the video
-        await getVideo(videoFile, filePath);
-
+filesRouter.post("/uploadFile", upload.single("video"), (req, res) => {
+    try {        
+        console.log(req.file);
         res.json({status: "OK", message: "File uploaded successfully"});
     } catch (error) {
         console.log("Error - unable to import video ", error);
