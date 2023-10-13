6c68aafaab549fa23b6684af93abe4722e66bbad
Emmey <emmey.leo@gmail.com>
2023-10-13 23:39:27 +1000

    Revert "FFMPEG middleware beginning"
    This reverts commit f5e8f624b800eef1b8baf27cc5e59ffa6ebc3222.

--- src/server/middleware/ffmpeg.ts
+++
@@ -1,8 +0,0 @@
-import { UploadedFile } from "express-fileupload";
-// import Ffmpeg from "fluent-ffmpeg";
-
-// this will do the resolution conversions
-// TODO: Update the resolution type
-async function convertVideoRes(videoFile: UploadedFile, resolution: string) {
-    console.log("Hello World");
-}
