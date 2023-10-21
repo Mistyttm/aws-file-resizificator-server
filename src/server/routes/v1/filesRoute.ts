fca996a43f52c49d573b96c7c7206ab480041b71
Mistyttm <emmey.leo@gmail.com>
2023-10-21 22:13:10 +1000

    tempRes route

--- src/server/routes/v1/filesRoute.ts
+++ src/server/routes/v1/filesRoute.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import upload from "../../middleware/multer"
+import { pathToFileURL } from "bun";
+import encodeVideo from '../../middleware/ffmpeg';
 
 export const filesRouter = Router();
 
@@ -13,3 +15,27 @@ filesRouter.post("/uploadFile", upload.single("video"), (req, res) => {
         res.status(500).json({ status: "Error", message: "Unable to import video" });
     }
 });
+
+filesRouter.post("/tempRes", async (req, res) => {
+    if (!req.body.video){
+        res.json({status: "error", message: "You have not provided a video file"});
+    }
+    if(!pathToFileURL("../../videos/" + req.body.video.toString())){
+        res.status(500).json({status: "error", message: "File does not exist"});
+    }
+    if (!req.body.resolution){
+        res.status(500).json({status: "error", message: "You have not provided adequate resolutions"});
+    }
+
+    const video = req.body.video;
+    const resolution = req.body.resolution;
+
+    try {
+        await encodeVideo(video, resolution).then(response => {
+            res.json({status: "OK", message: "Encoding complete", file: response});
+        });
+    }
+    catch (error) {
+        res.status(500).json({status: "error", message: error})
+    }
+})
