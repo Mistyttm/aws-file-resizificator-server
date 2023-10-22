875efe264e89bfa735ab7ff469e5f08b3d65e794
Mistyttm <emmey.leo@gmail.com>
2023-10-22 19:05:44 +1000

    change paths to be new paths

--- src/middleware/multer.ts
+++ src/middleware/multer.ts
@@ -3,7 +3,7 @@ import path from "path";
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'src/server/videos');
+        cb(null, 'src/videos');
     },
     filename: (req, file, cb) => {
         cb(null, Date.now() + '-' + file.originalname);
