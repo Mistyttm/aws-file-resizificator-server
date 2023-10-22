e9ddd6ff944cebf025981598d197d34ef443168d
Mistyttm <emmey.leo@gmail.com>
2023-10-22 19:01:04 +1000

    move source to /src

---
+++ src/middleware/multer.ts
@@ -0,0 +1,27 @@
+import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'src/server/videos');
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+});
+
+/* Check file has valid video extension type */ 
+const isVideo = function (req: any, file: Express.Multer.File, cb: any) {
+    const validExtensions = ['.mp4', '.mov', '.avi', '.mkv'];
+    const fileExtension = path.extname(file.originalname).toLowerCase();
+
+    if (validExtensions.includes(fileExtension)) {
+        cb(null, true);
+        } else { // If the file extension was invalid
+        cb(false);
+    }
+}; 
+
+const upload = multer({storage: storage, fileFilter: isVideo});
+
+export default upload;
