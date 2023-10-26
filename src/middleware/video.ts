53b1693385162a0147121034baca4d2cc84a37d7
Mistyttm <emmey.leo@gmail.com>
2023-10-26 21:52:05 +1000

    begin porting Belle's changes

--- src/middleware/video.ts
+++ src/middleware/video.ts
@@ -1,5 +1,9 @@
+import ffmpegPath from "ffmpeg-static";
+import Ffmpeg from "fluent-ffmpeg";
 
-export default async function getVideo(videoFile: any, filePath: string) {
+Ffmpeg.setFfmpegPath(ffmpegPath ?? "");
+
+export async function getVideo(videoFile: any, filePath: string) {
     console.log(videoFile);
     videoFile.mv(filePath, function (error: any) {
         if (error) { 
@@ -8,5 +12,26 @@ export default async function getVideo(videoFile: any, filePath: string) {
             console.log("Video imported");
             //todo: need to add a handler to validate the file is a video type
         }
+    });
+}
+
+// Todo: Probably add uploads back to s3 directly in encoding function
+export function encodeVideo(s3Url: string, outputName: string, resolution: string) {
+    return new Promise((resolve, reject) => {
+        Ffmpeg()
+            .input(s3Url) 
+            .videoCodec("libx264")
+            .size(resolution)
+            .output(outputName + ".mp4") //todo: add filename from mimetype, might use regex to format from "video/mp4" to ".mp4"
+            .on("end", (video) => {
+                // todo: add s3 upload here
+                console.log("Video encoding complete");
+                resolve(video);
+            })
+            .on("error", (error) => {
+                console.error("Error encoding video: ", error);
+                reject(error);
+            })
+            .run();
     });
 }
