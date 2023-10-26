aa0d774bbfc9dc9337032fe819d8d1bc3cb84307
Mistyttm <emmey.leo@gmail.com>
2023-10-26 22:38:31 +1000

    uploading

--- src/middleware/multer.ts
+++ src/middleware/multer.ts
@@ -1,7 +1,14 @@
 import multer from "multer";
 import path from "path";
 
-const storage = multer.memoryStorage();
+// @ts-ignore
+const storage = multer.memoryStorage({
+    // @ts-ignore
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+});
+
 
 /* Check file has valid video extension type */ 
 const isVideo = function (req: any, file: Express.Multer.File, cb: any) {
