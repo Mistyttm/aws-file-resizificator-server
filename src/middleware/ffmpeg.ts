4bcc6cc0f41609e43b843fa79ec1d29c18a66f2d
Mistyttm <emmey.leo@gmail.com>
2023-11-07 20:40:21 +1000

    Update ffmpeg

--- src/middleware/ffmpeg.ts
+++ src/middleware/ffmpeg.ts
@@ -1,30 +1,62 @@
 import Ffmpeg from "fluent-ffmpeg";
 import ffmpegPath from "ffmpeg-static";
 import fs from 'fs';
+import path from 'path';
+import { uploadToS3, getSignedUrl } from "./aws/s3Bucket";
+
+const bucketName = process.env.BUCKET_NAME;
 
 Ffmpeg.setFfmpegPath(ffmpegPath ?? "");
 
-/*Todo: Probably add uploads back to s3 directly in encoding function */
-export function encodeVideo(s3Url: string, outputName: string, resolution: string) {
-    console.log(s3Url);
+export async function encodeVideo(filePath: string, outputName: string, resolution: string, fileType: string) {
     return new Promise((resolve, reject) => {
-        Ffmpeg()
-            .input(s3Url) 
-            .videoCodec("libx264")
+        console.log("encoding video...");
+        const outputFilePath = path.join('encoded', outputName);
+    
+        Ffmpeg(filePath)
+            .videoCodec('libx264')
             .size(resolution)
-            .output(outputName + ".mp4") //todo: add filename from mimetype, might use regex to format from "video/mp4" to ".mp4"
-            .on("end", (video) => {
-                // todo: add s3 upload here
-                console.log("Video encoding complete");
-                resolve(video);
-            })
-            .on("error", (error) => {
-                console.error("Error encoding video: ", error);
+            .output(outputFilePath)
+            .on('end', async () => {
+            console.log('Video encoding complete.');
+    
+            // Upload the encoded video to S3
+            const fileStream = fs.createReadStream(outputFilePath);
+            const uploadParams = {
+                Bucket: bucketName,
+                Key: path.basename(outputFilePath),
+                Body: fileStream,
+                ContentType: fileType
+            };
+            try {
+                await uploadToS3(uploadParams);
+                // Get signed s3 url
+                const signedUrlParams = {
+                Bucket: bucketName,
+                Key: path.basename(outputFilePath),
+                Expires: 60 * 20 // url valid for 20 minutes
+                };
+
+                // Upload encoded video to s3 url
+                const url = await getSignedUrl(signedUrlParams);
+                console.log("Signed URL: ", url);
+    
+                // Delete file from disk storage
+                fs.unlinkSync(outputFilePath);
+                resolve(url);
+
+            } catch (error) {
+                console.error('Error uploading to S3: ', error);
                 reject(error);
+            }
+            })
+            .on('error', (error) => {
+            console.error('Error encoding video: ', error);
+            reject(error);
             })
             .run();
-    });
-}
+        });
+    }
 
 export async function getThumbnail(videoFile: any) {
     try {
