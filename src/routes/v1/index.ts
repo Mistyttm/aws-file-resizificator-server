e72640ac1f24c2e28f4e3908ca77c913f246eb44
Mistyttm <emmey.leo@gmail.com>
2023-11-07 21:37:16 +1000

    fix upload route

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -4,3 +4,6 @@ import { filesRouter } from './filesRoute';
 export const routes = express.Router();
 
 routes.use("/files", filesRouter);
+routes.use("/test", (req, res) => {
+    res.json({status: "OK", message: "running"});
+})
