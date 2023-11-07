3e85488766a1fee0e3cbee22f97254d12b1267d3
Mistyttm <emmey.leo@gmail.com>
2023-11-07 20:25:24 +1000

    update multer

--- src/middleware/multer.ts
+++ src/middleware/multer.ts
@@ -1,27 +1,23 @@
 import multer from "multer";
 import path from "path";
 
-// @ts-ignore
-const storage = multer.memoryStorage({
-    // @ts-ignore
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + '-' + file.originalname);
+/* Upload files to disk storage */
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads');
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname));
     }
 });
 
-
 /* Check file has valid video extension type */ 
-const isVideo = function (req: any, file: Express.Multer.File, cb: any) {
+const fileFilter = function (req: any, file: Express.Multer.File, cb: any) {
     const validExtensions = ['.mp4', '.mov', '.avi', '.mkv'];
     const fileExtension = path.extname(file.originalname).toLowerCase();
-
-    if (validExtensions.includes(fileExtension)) {
-        cb(null, true);
-        } else { // If the file extension was invalid
-        cb(false);
-    }
+    cb(null, validExtensions.includes(fileExtension));
 }; 
 
-const upload = multer({storage: storage, fileFilter: isVideo});
+const upload = multer({ storage: storage, fileFilter: fileFilter });
 
 export default upload;
