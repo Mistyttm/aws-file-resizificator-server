b587d6004d456dedc3537867ecf529334ea883b0
Mistyttm <emmey.leo@gmail.com>
2023-11-07 21:37:10 +1000

    fix multer output

--- src/middleware/multer.ts
+++ src/middleware/multer.ts
@@ -4,7 +4,7 @@ import path from "path";
 /* Upload files to disk storage */
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'uploads');
+        cb(null, 'output/uploads');
     },
     filename: function (req, file, cb) {
         cb(null, Date.now() + path.extname(file.originalname));
