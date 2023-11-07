03297888abdecd957539135807763ee45369c6b1
Mistyttm <emmey.leo@gmail.com>
2023-11-07 21:35:57 +1000

    fix index

--- src/index.ts
+++ src/index.ts
@@ -31,21 +31,21 @@ Ffmpeg.setFfmpegPath(ffmpegPath ?? "");
 // Routing
 app.use('/api/v1', routes);
 
-app.use(function(req, res, next) {
-    next(createHttpError(404));
-});
+// app.use(function(req, res, next) {
+//     next(createHttpError(404));
+// });
 
-// error handler
-// @ts-ignore
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // error handler
+// // @ts-ignore
+// app.use(function(err, req, res, next) {
+//     // set locals, only providing error in development
+//     res.locals.message = err.message;
+//     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
+//     // render the error page
+//     res.status(err.status || 500);
+//     res.render('error');
+// });
 
 /*Create s3 bucket and sqs queue (if not already created) 
 upload bucket data and retrieve the bucket object */
